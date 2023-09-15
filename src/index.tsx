a8c271a858b464502e8043bd3246957ff0d66054
Rahul keshri <rkeshri2021@gmail.com>
2023-09-15 14:34:53 +0530

    Setup Hasura and GraphQL Done

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  HttpLink,
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  link: new HttpLink({
+    uri: "https://evolving-fowl-65.hasura.app/v1/graphql",
+    headers: {
+      "Content-Type": "application/json",
+      "x-hasura-admin-secret":
+        "5acGklXa4Ag73Oaik5K85Kzj2gA6NhXCQB2YTxBJO8PBd1g0EjMjIcDP6EzUsUIF",
+    },
+  }),
+  cache: new InMemoryCache(),
+});
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
-
