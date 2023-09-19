e01cd20ff9db2970f97a8f324a2d785ffe1dfd14
Rahul keshri <rkeshri2021@gmail.com>
2023-09-20 01:29:57 +0530

    Done

--- src/Header/Title.tsx
+++ src/Header/Title.tsx
@@ -1,8 +1,11 @@
 import { Link } from "react-router-dom";
+import useTheme from "../Hooks/useTheme";
+
 const Title = () => {
+  const styles = useTheme();
   return (
     <div className="title">
-      <Link to={"/"}>
+      <Link to={"/"} style={styles}>
         <h1> YTS.In</h1>
       </Link>
     </div>
