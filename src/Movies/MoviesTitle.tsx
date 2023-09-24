597763dd6c221b7438ac20a2d9f79f27ba5fe336
Rahul keshri <rkeshri2021@gmail.com>
2023-09-24 21:58:23 +0530

    Add Some Styling

--- src/Movies/MoviesTitle.tsx
+++ src/Movies/MoviesTitle.tsx
@@ -1,14 +1,32 @@
 import { MdLocalMovies } from "react-icons/md";
+import styled from "styled-components";
+import useTheme from "../Hooks/useTheme";
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  font-size: 1.5rem;
+  font-weight: bold;
+  @media (max-width: 700px) {
+    font-size: 1rem;
+    gap: 0.5rem;
+  }
+`;
+
+const Hr = styled.hr<{ $color: string }>`
+  border: 2px solid ${(props) => props.$color};
+  border-radius: 1rem;
+`;
 
 const MoviesTitle = ({ numberOfMovies }: { numberOfMovies: number }) => {
+  const theme = useTheme();
   return (
     <section className="moviesTitle">
-      <div>
-        <h2>
-          <MdLocalMovies /> Latest Movies : {numberOfMovies} Available
-        </h2>
-        <hr />
-      </div>
+      <Wrapper>
+        <MdLocalMovies /> Latest Movies : {numberOfMovies} Available
+      </Wrapper>
+      <Hr $color={theme.color} />
     </section>
   );
 };
