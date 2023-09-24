151131b8385be1665ce71111c780d6cc6c8b2898
Rahul keshri <rkeshri2021@gmail.com>
2023-09-24 23:45:05 +0530

    Add Hero Slider Using Swiper Package

--- src/util/constant.ts
+++ src/util/constant.ts
@@ -2,3 +2,6 @@ export const V_INFO = `Welcome to the official V-Enjoy, your premier online movi
 web app. Explore a vast collection of high-quality movies in 720p,
 1080p, 2160p 4K, and immersive 3D experiences, all available in
 compact file sizes. Discover the world of V-Enjoy Movies today!`;
+
+export const V_INFO_Two = `Watch Your Favorite Hindi Movies here.Welcome to the official V-Enjoy, your premier online movie streaming
+web app`;
